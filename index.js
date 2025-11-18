61e28a5cd8eba1f63ffaf2966c3b10706e23b423
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-18 08:02:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const calculateTotal = (subtotal, shipping = 2.50) => {
+function calculateTotal = (subtotal, shipping = 2.50) => {
   return subtotal + shipping;
 };
 
 
-const printTopThreeHeadlines = (...headlines) => {
+function printTopThreeHeadlines = (...headlines) => {
   return headlines.slice(0, 3).join('\n');
 };
 
 
 
 
-const newsHeadlines = [
+function newsHeadlines = [
   'a',
   'b',
   'c'
   ];
   
   
+
 
 
 
